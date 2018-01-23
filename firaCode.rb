02097d9c77656b66b273a6e3e035a1245b85ca9a
Tristan Sokol <tristans@squareup.com>
2018-01-23 14:58:04 -0800

    improve FiraCode dep to actually check if FiraCode is installed

--- firaCode.rb
+++ firaCode.rb
@@ -4,18 +4,19 @@ end
 
 # I do not understand how this works, need to test on new machine?
 dep 'FiraCode-font' do
+  met? shell 'system_profiler SPFontsDataType | grep FiraCode'
   # source 'https://github.com/tonsky/FiraCode.git'
 end
 
 dep 'atom preferences' do
-  met? File.read(File.expand_path('~/.atom/config.cson')).include? "fontFamily: \"FiraCode-Light\""
+  met? File.read(File.expand_path('~/.atom/config.cson')).include? 'fontFamily: "FiraCode-Light"'
 end
 
 # doesn't seem to permanently change it? Also met? doesn't make any sense.
 dep 'terminal preferences' do
-  met? shell "defaults read com.apple.Terminal"
-  meet {log "changing font to Didot"
-    shell "osascript -e 'tell application"+" \"Terminal\" to set the font name of window 1"+" to \"Fira Code Retina\"'"
-  }
-
+  met? shell 'defaults read com.apple.Terminal'
+  meet do
+    log 'changing font to Didot'
+    shell "osascript -e 'tell application" + ' "Terminal" to set the font name of window 1' + " to \"Fira Code Retina\"'"
+  end
 end
