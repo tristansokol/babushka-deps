88600981b1ab8a08da1596cbb8c4f935449db503
Tristan Sokol <tristans@squareup.com>
2018-02-02 08:44:41 -0800

    add hexfiend as an app

--- apps.rb
+++ apps.rb
@@ -1,3 +1,10 @@
+dep 'apps' do
+  requires 'Postman.app',
+        'Spectacle.app',
+        'LastPass.app',
+        'Hex Fiend.app'
+end
+
 dep 'Postman.app' do
   source 'https://dl.pstmn.io/download/latest/osx'
 end
@@ -8,4 +15,8 @@ end
 
 dep 'LastPass.app' do
   source 'https://itunes.apple.com/us/app/lastpass/id926036361?ls=1&mt=12'
+end
+
+dep 'Hex Fiend.app'do
+  source 'http://ridiculousfish.com/hexfiend/files/Hex_Fiend_2.5.dmg'
 end
