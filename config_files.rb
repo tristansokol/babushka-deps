5a8ec669a3d99a61b9e545cfd262e58b6d29f228
Tristan Sokol <tristan.sokol@gmail.com>
2018-02-01 20:42:11 -0800

    improve the message on this dep

--- config_files.rb
+++ config_files.rb
@@ -2,7 +2,7 @@ dep 'config-files' do
   met? { File.exist?("#{ENV['HOME']}/.bash_profile") && File.exist?("#{ENV['HOME']}/.bashrc") }
   meet do
     message = <<-MESSAGE.gsub(/[\n\s]+/, ' ')
-    Your home directory is missing either .bash_profile or .bashrc. You should install one.
+    Your home directory is missing either .bash_profile or .bashrc. You should install one that sources the bash_profile in the your dotfiles repo.
     MESSAGE
     log_shell(message, 'echo') # echo serves no purpose here - just a dummy command
   end
