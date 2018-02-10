2bdc27f2262a6e287c1b6977e1280115a59614ec
Tristan Sokol <git@tristansokol.com>
2018-02-09 20:49:50 -0800

    add duti

--- macPreferences.rb
+++ macPreferences.rb
@@ -1,26 +1,36 @@
-dep "mac-preferences" do
-  requires "dev-folder"
+dep 'mac-preferences' do
+  requires 'dev-folder',
+           'hotcorners',
+           'duti'
 end
-dep "hotcorners" do
-  #make the top left hotcorner make the computer go to sleep.
-  met?{
-     raw_shell("sudo defaults read com.apple.dock.plist wvous-tl-corner").stdout.strip! == "5"
-}
-  meet{
-    log_shell "Setting the top left corner to sleepify", "sudo defaults write com.apple.dock.plist wvous-tl-corner -int 5"
-    shell "sudo defaults write com.apple.dock.plist wvous-tl-modifier -int 0"
-    shell "sudo killall -hup cfprefsd"
-    shell "sudo killall -hup Dock"
-  }
-end
-
-dep 'dev-folder'do
-  met?{'~/Development'.p.exists?}
-  meet{
-    log_shell "making you a development folder",'mkdir ~/Development'
-  }
+dep 'hotcorners' do
+  # make the top left hotcorner make the computer go to sleep.
+  met? do
+    raw_shell('sudo defaults read com.apple.dock.plist wvous-tl-corner').stdout.strip! == '5'
+  end
+  meet do
+    # log_shell "Setting the top left corner to sleepify", "sudo defaults write com.apple.dock.plist wvous-tl-corner -int 5"
+    # shell "sudo defaults write com.apple.dock.plist wvous-tl-modifier -int 0"
+    # shell "sudo killall -hup cfprefsd"
+    # shell "sudo killall -hup Dock"
+  end
 end
 
-dep 'exuberant ctags'do
-  met?{}
+dep 'dev-folder' do
+  met? { '~/Development'.p.exists? }
+  meet do
+    log_shell 'making you a development folder', 'mkdir ~/Development'
+  end
+end
+
+dep 'exuberant ctags' do
+  met? {}
+end
+
+dep 'duti' do
+  met? { shell?('brew list | grep duti') }
+  meet do
+    log_shell "installing duti","brew install duti"
+    log_shell "duti-ing the duti file in ~/dotfiles","duti ~/dotfiles/dutifile.duti"
+  end
 end
