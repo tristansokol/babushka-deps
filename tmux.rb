d4a8bd9086e1f16e90802d9a6adb96ea11a12a7c
Tristan Sokol <tristans@squareup.com>
2018-05-08 09:12:47 -0700

    add tmuxinator deps

--- tmux.rb
+++ tmux.rb
@@ -14,3 +14,18 @@ dep 'tmux-conf' do
     log_shell 'symlinking .tmux.conf from dotfiles repo', 'ln -s ~/dotfiles/tmux.conf ~/.tmux.conf'
   end
 end
+
+dep 'tmuxinator' do
+  requires  'tmux'
+  met? { 'which tmuxinator' }
+  meet do
+    log_shell 'installing tmuxinator', 'gem install tmuxinator'
+  end
+end
+
+dep 'tmuxinator-storage' do
+  met? { '~/.tmuxinator'.p.exists? }
+  meet do
+    log_shell 'symlinking ~/.tmuxinator from dotfiles repo', 'ln -s ~/dotfiles/tmuxinator ~/.tmuxinator'
+  end
+end
