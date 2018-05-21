14ab94457d2b22a2803b734b7e9579632bcf9f0b
Tristan Sokol <tristans@squareup.com>
2018-05-21 09:25:36 -0700

    add some vscode packages

--- vscode.rb
+++ vscode.rb
@@ -1,6 +1,10 @@
 dep 'vscode' do
-  requires 'Visual Studio Code.app', 'vscode-settings-symlinked',
-  'vscode-keybindings-symlinked'
+  requires 'Visual Studio Code.app',
+           'vscode-settings-symlinked',
+           'vscode-keybindings-symlinked',
+           'Duplicate.vscodeextension',
+           'Ruby.vscodeextension',
+           'Markdownlint.vscodeextension'
 end
 
 dep 'Visual Studio Code.app' do
@@ -14,7 +18,7 @@ dep 'vscode-settings-symlinked' do
   end
   meet do
     log_shell('copying old settings', 'cp -va $HOME/Library/Application\ Support/Code/User/settings.json "$HOME/Library/Application Support/Code/User/settings-$(date +"%Y%m%d-%H%M%S").json"')
-        log_shell 'Deleting old settings', 'rm -rf $HOME/Library/Application\ Support/Code/User/settings.json'
+    log_shell 'Deleting old settings', 'rm -rf $HOME/Library/Application\ Support/Code/User/settings.json'
     log_shell 'symlinking settings from dotfiles', 'ln -sf $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json'
   end
 end
@@ -25,7 +29,33 @@ dep 'vscode-keybindings-symlinked' do
   end
   meet do
     log_shell('copying old keybindings', 'cp -va $HOME/Library/Application\ Support/Code/User/keybindings.json "$HOME/Library/Application Support/Code/User/keybindings-$(date +"%Y%m%d-%H%M%S").json"')
-        log_shell 'Deleting old keybindings', 'rm -rf $HOME/Library/Application\ Support/Code/User/keybindings.json'
+    log_shell 'Deleting old keybindings', 'rm -rf $HOME/Library/Application\ Support/Code/User/keybindings.json'
     log_shell 'symlinking keybindings from dotfiles', 'ln -sf $HOME/dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json'
   end
+end
+
+meta :vscodeextension do
+  accepts_value_for :name
+  template do
+    requires 'Visual Studio Code.app'
+    met? do
+      log_shell "seeing if the Visual Studio Code extension #{name} is intalled", "code --list-extensions | grep -i #{name}"
+    end
+    meet do
+      log_shell "installing the #{name} extension", "code --install-extension #{name}"
+    end
+  end
+end
+
+dep 'Duplicate.vscodeextension' do
+  name 'mrmlnc.vscode-duplicate'
+end
+
+dep 'Ruby.vscodeextension' do
+  name 'rebornix.ruby'
+end
+
+
+dep 'Markdownlint.vscodeextension' do
+  name 'DavidAnson.vscode-markdownlint'
 end
