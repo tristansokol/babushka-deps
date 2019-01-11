92c9a3cc60f7d5a452d3731862199c89f0b5bcee
Tristan Sokol <tristansokol@users.noreply.github.com>
2019-01-11 11:39:16 -0600

    get rid of hot corners

--- macPreferences.rb
+++ macPreferences.rb
@@ -73,18 +73,18 @@ dep 'printPanel' do
     shell('defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true')
   end
 end
-dep 'hotcorners' do
-  # make the top left hotcorner make the computer go to sleep.
-  met? do
-    raw_shell('sudo defaults read com.apple.dock.plist wvous-tl-corner').stdout.strip! == '5'
-  end
-  meet do
-    # log_shell "Setting the top left corner to sleepify", "sudo defaults write com.apple.dock.plist wvous-tl-corner -int 5"
-    # shell "sudo defaults write com.apple.dock.plist wvous-tl-modifier -int 0"
-    # shell "sudo killall -hup cfprefsd"
-    # shell "sudo killall -hup Dock"
-  end
-end
+# dep 'hotcorners' do
+#   # make the top left hotcorner make the computer go to sleep.
+#   met? do
+#     raw_shell('sudo defaults read com.apple.dock.plist wvous-tl-corner').stdout.strip! == '5'
+#   end
+#   meet do
+#     # log_shell "Setting the top left corner to sleepify", "sudo defaults write com.apple.dock.plist wvous-tl-corner -int 5"
+#     # shell "sudo defaults write com.apple.dock.plist wvous-tl-modifier -int 0"
+#     # shell "sudo killall -hup cfprefsd"
+#     # shell "sudo killall -hup Dock"
+#   end
+# end
 
 dep 'dev-folder' do
   met? { '~/Development'.p.exists? }
